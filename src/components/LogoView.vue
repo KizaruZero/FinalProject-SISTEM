aa59d4c299ad0c21618af9563d7257fb5858da79
Ardya Pusaka <143238697+KizaruZero@users.noreply.github.com>
2024-11-01 15:44:34 +0700

    UP

--- src/components/LogoView.vue
+++ src/components/LogoView.vue
@@ -5,19 +5,19 @@
       <h1 class="text-2xl font-bold">Featured in</h1>
     </div>
     <div class="logo-1">
-      <img src="@/assets/logo 1.png" alt="" />
+      <img src="@/assets/Logo 1.png" alt="" />
     </div>
     <div class="logo-2">
-      <img src="@/assets/logo 2.png" alt="" />
+      <img src="@/assets/Logo 2.png" alt="" />
     </div>
     <div class="logo-3">
-      <img src="@/assets/logo 3.png" alt="" />
+      <img src="@/assets/Logo 3.png" alt="" />
     </div>
     <div class="logo-4">
-      <img src="@/assets/logo 4.png" alt="" />
+      <img src="@/assets/Logo 4.png" alt="" />
     </div>
     <div class="logo-5">
-      <img src="@/assets/logo 5.png" alt="" />
+      <img src="@/assets/Logo 5.png" alt="" />
     </div>
   </div>
 </template>
